4cb24bc012cb8ea0fe6a6f443631ec217a805c59
Yoodahun <dahun4032@gmail.com>
2021-03-14 01:39:41 +0900

    ADD tags

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = "stepDefinition",
-        tags = "@RegressionTest or @User or @login"
+        tags = "@RegressionTest or @User or @login or @Store or @Item"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
